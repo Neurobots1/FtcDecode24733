cd8f76c2dd86c84815a60080e1ea797e6f562557
Neurobots1 <neurobots@classomption.qc.ca>
2026-01-13 14:44:34 -0500

    Test code

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/TestCode.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.robotcontroller.internal;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+public class TestCode extends OpMode {
+    @Override
+    public void init (){
+
+
+    }
+
+    @Override
+    public void loop (){
+
+        String McDo = "Xavier is ____";
+telemetry.addLine("McDo") ;
+
+
+    }
+}
